8b6e0c6b8d4daa9db883640150bf0045833eadfe
ivandavid858 <ivandavid858@gmail.com>
2024-11-18 19:53:52 +0100

    First Commit

---
+++ src/main/java/com/tests/system/TestsSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tests.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tests/system/TestsSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
